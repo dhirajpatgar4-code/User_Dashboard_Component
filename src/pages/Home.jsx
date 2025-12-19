fab44b9fba6bee11a843eb31d92bb857d17049b5
jaskirat <kiratsingh5879@gmail.com>
2025-12-19 23:28:22 +0530

    feat: Hero Section UI component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import { Footer, Navbar } from "../components";
+import HeroSection from "../components/HeroSection";
 import Testimonials from "../components/ui/testimonials/Testimonials";
 
 const Home = () => {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Navbar />
-			<main className="grow"></main>
+			<main className="grow">
+				<HeroSection/>
+			</main>
 			<Footer />
 		</div>
+		
 	);
 };
 
